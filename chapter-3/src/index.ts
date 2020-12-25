4766bb62e2bd6318ecdbfcd7b43016078462d5da
akira ogawa <oakira@outlook.com>
2020-12-25 15:57:20 +0900

    ko_hotel.css調整

--- chapter-3/src/index.ts
+++ chapter-3/src/index.ts
@@ -11,11 +11,25 @@ if (typeof a2 === 'number') {
 } else if (typeof a2 === 'string') {
 	let e2 = a2 + ' is string'
 }
+
 //boolean
 let a3 = true
-let b3 = false
+var b3 = false
 let d3: boolean = true
 //literal type
 const c3 = true
 let e3: true = true
 // let f3: true = false
+
+//number
+let a4 = 1234
+var b4 = Infinity * 0.10
+const c4 = 5678
+let d4 = a < b
+let e4: number = 100
+let f4: 26.218 = 26.218
+// let g4: 26.218 = 10
+
+//bigint
+//es2020以上がターゲットの場合のみ利用可能
+let a5 = 1234n
